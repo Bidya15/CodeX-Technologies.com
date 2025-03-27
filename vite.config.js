65035958e315b19f3162e0dc01d68d8bf3cd12b0
Bidya Sing Rongpi <bidyasingrongpi90@gmail.com>
2025-03-27 21:38:35 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: VITE_BASE_PATH || "/CodeX-Technologies.com",
   server: {
     proxy: {
       "/api": "http://localhost:5000", // Proxy /api requests to backend
